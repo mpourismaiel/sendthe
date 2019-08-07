45b3884b2d65d3e569a1e573ac50897472851339
Mahdi Pourismaiel <mpourismaiel@gmail.com>
2019-08-07 13:41:47 +0430

    Fix a problem with build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,13 +1,18 @@
-declare module 'types' {
-  export interface Dictionary<T> {
-    [key: string]: T;
-  }
+declare module "types" {
+    export interface Dictionary<T> {
+        [key: string]: T;
+    }
 }
-declare module 'sendthe' {
-  const SendThe: {
-    (name: any, params?: any, query?: any): any;
-    add(name: any, endpoint: any): void;
-  };
-  export default SendThe;
+declare module "sendthe" {
+    import { Dictionary } from "types";
+    const SendThe: (API?: {}) => {
+        get: (name: any, params?: any, query?: any) => any[] | null;
+        add: (name: any, endpoint: any) => void;
+        cache: Dictionary<string[]>;
+    };
+    export default SendThe;
 }
-declare module 'index.test' {}
+declare module "index.test" { }
+declare module "index" {
+    export { default as SendThe } from "sendthe";
+}
